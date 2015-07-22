087a86b0b8a7be48915a0ff458ef2aabc186e736
Jesse Shawl <jesse@jshawl.com>
2015-07-21 20:52:55 -0400

    clean

--- src/dabble.js
+++ src/dabble.js
@@ -1,10 +1,12 @@
 var fs = require("fs")
 module.exports = function(){
   console.log("Dabblin!")
-  console.log(process.cwd())
   var time = (new Date).getTime()
   var toFile = process.cwd() + "/dabble-" + time + ".html"
   fs.createReadStream(__dirname + "/index.html").pipe(fs.createWriteStream(toFile))
   var editor = require('child_process').spawn(process.env.EDITOR, [toFile], {stdio: 'inherit'})
-  editor.on('exit', process.exit)
+  editor.on('exit', function(){
+    console.log("dabbled")
+    process.exit()
+  })
 }
