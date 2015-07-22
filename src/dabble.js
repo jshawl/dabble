4e97a6529d3c949995f32e483b2756c4b95eb476
Jesse Shawl <jesse@jshawl.com>
2015-07-21 20:50:02 -0400

    open editor

--- src/dabble.js
+++ src/dabble.js
@@ -5,4 +5,6 @@ module.exports = function(){
   var time = (new Date).getTime()
   var toFile = process.cwd() + "/dabble-" + time + ".html"
   fs.createReadStream(__dirname + "/index.html").pipe(fs.createWriteStream(toFile))
+  var editor = require('child_process').spawn(process.env.EDITOR, [toFile], {stdio: 'inherit'})
+  editor.on('exit', process.exit)
 }
