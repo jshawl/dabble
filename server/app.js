b359df4e1452ad61e3be05259ee04534998af58d
Jesse Shawl <jesse@jshawl.com>
2015-07-25 14:03:55 +0000

    add versioning

--- server/app.js
+++ server/app.js
@@ -10,8 +10,25 @@ app.get("/", function(req, res){
   res.send("dabble!")
 })
 
+function version( file ){
+  if(file.match(/-v[0-9]+\.html$/)){
+    var num = parseInt(file.match(/-v([0-9]+)\.html$/)[1])
+    var name = file.replace(/[0-9]+\.html$/, ++num + ".html")
+  } else {
+    var name = file.replace(".html","-v1.html")
+  }
+  if(fs.fileExistsSync(name)){
+    version( name )
+  }
+  return name
+}
+
 app.post("/", function(req, res){
+  var toFile = __dirname + "/public/" + req.body.name
   if(req.body.name.match(/\.html$/)){
+    if(fs.fileExistsSync(toFile)){
+      toFile = version(toFile)
+    }
     fs.writeFile(__dirname+"/public/"+req.body.name, req.body.content, function(err){
       res.json({
 	status: "ok.",
